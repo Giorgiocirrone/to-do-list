6feee045503d5cd50f7c2fff0ca67d09f401b21f
Giorgiocirrone <cirronegiorgio@gmail.com>
2025-03-18 17:54:24 +0100

    To do list

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+
+/*scaletta di creazione  */
+//prendere gli elementi dal  DOM 
+
+const todoList = document.getElementById('todoList');
+const todoInput = document.getElementById('todoInput');
+const todoForm = document.getElementById('todoForm');
+
+
+
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const todoItem = document.createElement('li');
+    todoList.appendChild(todoItem);
+
+
+
+    const todoDescription = document.createElement('div');
+    todoDescription.textContent = todoInput.value;
+    todoItem.appendChild(todoDescription);
+
+
+
+
+    const todoBtnDone = document.createElement('button');
+    todoBtnDone.textContent = 'X';
+    todoBtnDone.addEventListener('click', () => {
+        todoItem.remove();
+
+    });
+
+    todoItem.appendChild(todoBtnDone);
+
+    todoInput.value = '';
+});
+
+
+
